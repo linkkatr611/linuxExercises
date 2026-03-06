f4b76ff7eca69fdd5381a23f62d3fd7baf22da1a
Yiran Lin <230067740+linkkatr611@users.noreply.github.com>
2026-03-05 22:01:43 -0600

    Revise school.sh

--- school.sh
+++ school.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
-cat Property_Tax_Roll.csv \
-| grep "MADISON SCHOOLS" \
-| cut -d',' -f7 \
-| { sum=0; n=0; while read v; do sum=$(($sum + $v)); n=$(($n + 1)); done; echo $(($sum / $n)); }
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d, -f7 | {
+  sum=0
+  n=0
+  while read x
+  do
+    sum=$((sum + x))
+    n=$((n + 1))
+  done
+  echo $((sum / n))
+}
