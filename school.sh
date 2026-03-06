42f3a71398f23ac73306af8bc800679e5a9feed6
Lei Tang <ltang79@wisc.edu>
2026-03-05 18:50:34 -0600

    school.sh average TotalAssessedValue for MADISON SCHOOLS

--- school.sh
+++ school.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+cat Property_Tax_Roll.csv \
+| grep "MADISON SCHOOLS" \
+| cut -d',' -f7 \
+| { sum=0; n=0; while read v; do sum=$(($sum + $v)); n=$(($n + 1)); done; echo $(($sum / $n)); }
